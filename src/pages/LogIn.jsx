1fae0d0ee0acaf65b05c005969f5a4f1a06a8b76
mldvPaul <mldvPaul@gmail.com>
2023-10-19 18:30:57 +0200

    logIn functionality

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
+import { useState } from 'react'
 
 const LogIn = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const {user, logIn} = UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        try {
+           await logIn(email, password) 
+           navigate('/')
+        } catch (error) {
+           console.log(error) 
+           setError(error.message)
+        }
+    }
+
   return (
     <div className='w-full h-screen'>
     <img className='hidden sm:block absolute w-full h-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/2cda6362-3a82-440d-9992-e7aedf879b15/DE-en-20231016-popsignuptwoweeks-perspective_alpha_website_small.jpg' alt='/'/>
@@ -10,9 +30,10 @@ const LogIn = () => {
             <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 rounded text-white'>
                <div className='max-w-[320px] mx-auto py-16'>
                     <h1 className='text-3xl font-bold'>Sign In</h1>
-                    <form className='w-full flex flex-col py-4'>
-                        <input className='p-3 m-2 rounded bg-gray-700' type="email" placeholder='Email' autoComplete='email'/>
-                        <input className='p-3 m-2 rounded bg-gray-700' type="password" placeholder='Password' autoComplete='current-password'/>
+                    {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
+                    <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                        <input onChange={(e) => setEmail(e.target.value)} className='p-3 m-2 rounded bg-gray-700' type="email" placeholder='Email' autoComplete='email'/>
+                        <input onChange={(e) => setPassword(e.target.value)} className='p-3 m-2 rounded bg-gray-700' type="password" placeholder='Password' autoComplete='current-password'/>
                         <button className='bg-red-600 py-3 mx-2 my-6 rounded font-bold'>Sign In</button>
                         <div className='flex justify-between items-center text-sm text-gray-600'>
                             <p><input className='mr-2' type='checkbox'/>Remember me</p>
