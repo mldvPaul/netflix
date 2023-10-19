92351b626677356226b655ce7bf06bfd99efbce1
mldvPaul <mldvPaul@gmail.com>
2023-10-19 16:16:51 +0200

    AuthContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
 import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import Signup from "./pages/Signup";
+
 
 
 function App() {
   return (
     <div className="App">
+      <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<Account />} />
       </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,6 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
