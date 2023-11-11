41d796c36b76d1eae7a4d1e88a26d984484aedb0
mldvPaul <mldvPaul@gmail.com>
2023-11-11 12:33:13 +0100

    save data for user in database

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,43 +1,100 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import { auth } from "../firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth, db } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import {setDoc,doc} from 'firebase/firestore'
 
 const AuthContext = createContext();
 
-export function AuthContextProvider({children}) {
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
 
-    const [user, setUser] = useState({});
-
-    function signUp (email, password) {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
-
-    function logOut() {
-        return signOut(auth)
-    }
-
-    function logIn(email, password) {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser)
-        })
-        return () => {
-            unsubscribe()
-        }
+  function signUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password);
+    setDoc(doc(db, 'users', email), {
+        savedShows: []
     })
+  }
 
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
 
-    return (
-        <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
-            {children}
-        </AuthContext.Provider>
+  function logOut() {
+    return signOut(auth);
+  }
 
-    )
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function UserAuth () {
-    return useContext(AuthContext)
+export function UserAuth() {
+  return useContext(AuthContext);
 }
+
+
+
+
+
+
+
+// import { createContext, useContext, useState, useEffect } from "react";
+// import { auth, db } from "../firebase";
+// import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+// import { setDoc, doc} from "firebase/firestore";  
+
+// const AuthContext = createContext();
+
+// export function AuthContextProvider({children}) {
+
+//     const [user, setUser] = useState({});
+
+//     function signUp (email, password) {
+//          createUserWithEmailAndPassword(auth, email, password);
+//          setDoc(doc(db, 'users', email), {
+//            savedShows: []   
+//          })
+//     }
+
+//     function logOut() {
+//         return signOut(auth)
+//     }
+
+//     function logIn(email, password) {
+//         return signInWithEmailAndPassword(auth, email, password)
+//     }
+
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//             setUser(currentUser)
+//         })
+//         return () => {
+//             unsubscribe()
+//         }
+//     })
+//     return (
+//         <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+//             {children}
+//         </AuthContext.Provider>
+
+//     )
+// }
+// export function UserAuth () {
+//     return useContext(AuthContext)
+// }
